8116285c55517063ac9746ee15b83c9d4fb1b4dc
yttyx <yttyx@picasa.org.uk>
2022-07-01 20:22:51 +0100

    Bring in the dictionary build code

--- src/dictionary.h
+++ src/dictionary.h
@@ -8,6 +8,7 @@
 
 #include "cmdparser.h"
 #include "dictionary.h"
+#include "distribution.h"
 #include "stenoflags.h"
 #include "symbols.h"
 #include "textfile.h"
@@ -19,13 +20,20 @@ namespace stenosys
 
 #define EMPTY 0xffffffff
 
+// Used by direct lookup mechanism (which will be replaced)
 typedef struct
 {
     std::string latin;
     uint16_t    latin_flags;
     std::string shavian;
     uint16_t    shavian_flags;
+} STENO_ENTRY_PREV;
+
+typedef struct {
+    std::string chord;
+    std::string text;
 } STENO_ENTRY;
+
 
 class C_dictionary : C_text_file
 {
@@ -34,6 +42,9 @@ public:
 
     C_dictionary();
     ~C_dictionary();
+
+    bool
+    build( const std::string & dictionary_path,  const std::string & output_path );   
 
     bool
     read( const std::string & path );
@@ -49,7 +60,47 @@ public:
 private:
 
     void
+    hash_map_initialise( uint32_t dictionary_count );
+    
+    bool
+    hash_map_build();
+
+
+    bool
+    hash_insert( const std::string & key, uint32_t dictionary_entry, uint32_t & collisions );
+
+    bool
+    hash_map_test();
+
+    bool
+    hash_map_report();
+
+    bool
+    hash_find( const std::string & key, std::string & value );  
+    
+    bool
+    get_dictionary_entry( uint32_t entry, std::string & steno, std::string & text );
+
+    uint32_t
+    generate_hash( const char * key );
+
+    bool
+    write( const std::string & output_path );
+
+    void
+    top( FILE * output_stream );
+
+    void
+    write( FILE * output_stream );
+
+    void
+    tail( FILE * output_stream );
+
+    void
     escape_characters( std::string & str );
+
+    std::string
+    get_filename( const std::string & path );
 
     bool
     parse_line( const std::string & line
@@ -67,9 +118,23 @@ private:
 
     std::string error_message_;
     
-    std::unique_ptr< std::unordered_map< std::string, STENO_ENTRY > >  dictionary_;
-    std::unique_ptr< C_cmd_parser >                                    parser_;
-    std::unique_ptr< C_symbols >                                       symbols_;
+    std::unique_ptr< std::unordered_map< std::string, STENO_ENTRY_PREV > >  dictionary_;
+    std::unique_ptr< C_cmd_parser >                                         parser_;
+    std::unique_ptr< C_symbols >                                            symbols_;
+
+    std::vector< STENO_ENTRY > dictionary_array_;
+
+    uint32_t * hashmap_;
+
+    uint32_t hash_capacity_;
+    uint32_t hash_entry_count_;
+
+    uint32_t hash_wrap_count_;
+    uint32_t hash_duplicate_count_;
+    uint32_t hash_hit_capacity_count_;
+
+    std::unique_ptr< C_distribution > hash_collision_distribution_;
+
 };
 
 }
