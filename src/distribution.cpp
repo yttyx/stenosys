92bb5882470082d8d0311cfd3f23b4e4269db387
yttyx <yttyx@picasa.org.uk>
2022-10-22 16:43:14 +0100

    Revise dictionary map report for build output

--- src/distribution.cpp
+++ src/distribution.cpp
@@ -7,6 +7,7 @@ namespace stenosys
 static const uint32_t BAR_WIDTH          = 126;
 static const uint32_t BUCKET_DISPLAY_MAX = 32;
 
+#define COLUMN_WIDTH    7
 #define COLUMN_MAX( x ) ( std::min( x, BUCKET_DISPLAY_MAX ) )
 
 
@@ -67,7 +68,7 @@ C_distribution::report()
 
     for ( uint32_t bucket = 0; bucket <= COLUMN_MAX( max_bucket_ ); bucket++ )
     {
-        snprintf( buffer, sizeof( buffer ), "%6u", bucket );
+        snprintf( buffer, sizeof( buffer ), "%*u", COLUMN_WIDTH, bucket );
         report += buffer;
     }
 
@@ -77,7 +78,7 @@ C_distribution::report()
     {
         uint32_t score = buckets_[ bucket ];
 
-        snprintf( buffer, sizeof( buffer ), "%6u", score );
+        snprintf( buffer, sizeof( buffer ), "%*u", COLUMN_WIDTH, score );
         report += buffer;
     }
 
@@ -87,69 +88,25 @@ C_distribution::report()
     {
         double fraction_of_total = ( 100.0 * ( double ) buckets_[ bucket ] ) / ( double ) total_score;
 
-        snprintf( buffer, sizeof( buffer ), "%6s", format_percentage( fraction_of_total ).c_str() );
+        snprintf( buffer, sizeof( buffer ), "%*s", COLUMN_WIDTH, format_percentage( fraction_of_total ).c_str() );
+        report += buffer;
+    }
+
+    report += "\nU: ";
+
+    double cumulative_fraction = 0.0;
+
+    for ( uint32_t bucket = 0; bucket <= COLUMN_MAX( max_bucket_ ); bucket++ )
+    {
+        double fraction_of_total = ( 100.0 * ( double ) buckets_[ bucket ] ) / ( double ) total_score;
+        
+        cumulative_fraction += fraction_of_total;
+
+        snprintf( buffer, sizeof( buffer ), "%*s", COLUMN_WIDTH, format_percentage( cumulative_fraction ).c_str() );
         report += buffer;
     }
 
     report += "\n";
-
-
-
-        //double fraction_of_total   = ( 100.0 * ( double ) score ) / ( double ) total_score;
-        //cumulative_fraction += fraction_of_total;
-        
-        //const char * marker = ( bucket == max_used_bucket_ ) ? "M" : " ";
-
-        //char bucket_desc[ 20 ];
-
-        //uint32_t bucket_from = ( bucket * ( uint32_t ) bucket_width_ );
-        //uint32_t bucket_to   = ( ( ( bucket + 1 ) * ( uint32_t ) bucket_width_ ) - 1 );
-
-        //if ( bucket_from == bucket_to )                                      
-        //{
-            //snprintf( bucket_desc, sizeof( bucket_desc ), "%7u", bucket_from );
-        //}
-        //else
-        //{
-            //snprintf( bucket_desc, sizeof( bucket_desc ), "%3u-%3u", bucket_from, bucket_to );
-        //}
-
-        //if ( bucket <= max_bucket_ )
-        //{
-            //snprintf( buffer, sizeof( buffer ), "%-7s   %s[%5u][%6s%%][%6s%%]| "
-                                                //, bucket_desc
-                                                //, marker
-                                                //, score
-                                                //, format_percentage( fraction_of_total ).c_str()
-                                                //, format_percentage( cumulative_fraction ).c_str() );
-            //report += buffer;
-
-            //uint32_t bar_length = ( score * BAR_WIDTH ) / max_score;
-
-            //if ( ( bar_length == 0 ) && ( score > 0 ) )
-            //{
-                //bar_length = 1;
-            //}
-
-            //snprintf( buffer, sizeof( buffer), "%s\n", std::string( bar_length, '-' ).c_str() );
-            //report += buffer;
-        //}
-        //else
-        //{
-            //snprintf( buffer, sizeof( buffer), "%3.3s       %s[%5u][%6s%%][%6s%%]| ", ">"
-                                                //, marker
-                                                //, score
-                                                //, format_percentage( fraction_of_total ).c_str()
-                                                //, format_percentage( cumulative_fraction ).c_str() );
-            //report += buffer;
-        //}
-    //}
- 
-    //snprintf( buffer, sizeof( buffer), "\n" );
-    //report += buffer;
-    
-    //snprintf( buffer, sizeof( buffer), "Total:     [%5u]| \n", total_score );
-    //report += buffer;
 
     return report;
 }
